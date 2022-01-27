070ded65e37808a98d3048ff38f5b21a0d879ed7
kyrisss <kornev.roma62@gmail.com>
2022-01-27 09:08:32 +0300

    feat: done task

--- src/index.js
+++ src/index.js
@@ -38,9 +38,39 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
-}
+    let arr =[];
+    let res ='';
+    for(let i =0; i < expr.length; i += 10){
+        arr.push(expr.slice(i,i+10))
+    }
+    
+    arr.forEach(item => {
+        //console.log(item)
+        if(item == "**********"){
+            res += ' ';
+        }
+        else{
+            let str = '';
+            for(let j = 0; j<10; j+=2){
+                
+                switch(item.slice(j,j+2)){
+                    case "00":
+                        //str += "";
+                        break;
+                    case "10":
+                        str += ".";
+                        break;
+                    case "11":
+                        str += "-"
+                        break;
+                }
+            }
+            res += MORSE_TABLE[str]
+        }
+    }) 
+    return res;
 
+}
 module.exports = {
     decode
 }
